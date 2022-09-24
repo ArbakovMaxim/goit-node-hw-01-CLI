1d7d36fe36a7ad2fbedea75691a5c4be99aa9718
Arbakov Maxim <fefaodessa@gmail.com>
2022-09-24 15:10:13 +0300

    DZ 1 Node start

---
+++ contacts.js
@@ -0,0 +1,49 @@
+const fs = require("fs").promises;
+const path = require("path");
+
+const contactsPath = path.join(__dirname, "/bd/contacts.json");
+
+const updateContacts = async (allContacts) =>
+  await fs.writeFile(contactsPath, JSON.stringify(allContacts, null, 2));
+
+const listContacts = async () => {
+  const data = await fs.readFile(contactsPath);
+  return JSON.parse(data);
+};
+
+const getContactById = async (contactId) => {
+  const contact = await listContacts();
+  const result = contact.find((item) => item.id === contactId);
+  return result || null;
+};
+
+const removeContact = async (contactId) => {
+  const allContacts = await listContacts();
+  const index = allContacts.findIndex((item) => item.id === contactId);
+  if (index === -1) {
+    return null;
+  }
+  const [result] = allContacts.splice(index, 1);
+  await updateContacts(allContacts);
+  return result;
+};
+
+const addContact = async (name, email, phone) => {
+  const allContacts = await listContacts();
+  const newContact = {
+    id: allContacts.length + 1,
+    name,
+    email,
+    phone,
+  };
+  allContacts.push(newContact);
+  await updateContacts(allContacts);
+  return newContact;
+};
+
+module.exports = {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+};
---
+++ index.js
@@ -0,0 +1,26 @@
+const contacts = require("./contacts");
+
+const invokeAction = async ({ action, id, name, email, phone }) => {
+  switch (action) {
+    case "list":
+      const allContacts = await contacts.listContacts();
+      console.log(allContacts);
+    case "get":
+      const oneContact = await contacts.getContactById(id);
+      break;
+
+    case "add":
+      // ... name email phone
+      break;
+
+    case "remove":
+      // ... id
+      break;
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+};
+
+//invokeAction({ action: "list" });
+invokeAction({ action: "list", id: "2" });
